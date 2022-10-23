96342da7bdf5807e4d6c09458bc09d6ff255f560
KIRILL ZLOKAZOV <100695130+EvilKloz@users.noreply.github.com>
2022-10-23 23:06:53 +0300

    Add files via upload

---
+++ Vtitbid.ISP20.SarbaevZlokazov.RND/OwnRandom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vtitbid.ISP20.SarbaevZlokazov.RND
+{
+    public static class OwnRandom
+    {
+        static public int Next(int first, int second)
+        {
+            Thread.Sleep(120);
+            double seconds = DateTime.Now.Second;
+            int milliseconds = DateTime.Now.Millisecond;
+            int dop = Convert.ToString(second).Length;
+            int random = Convert.ToInt32(Math.Pow(seconds, 3)) * milliseconds / 3* (dop*10);
+            int result = 0;
+            int show = second - first + 1;
+            int[] choices = new int[show];
+            choices[0] = first;
+            for (int i = 1; i < choices.Length; i++)
+            {
+                choices[i] = choices[i - 1] + 1;
+            }
+            int g = 0;
+            for (int i = 1; i < random; i++)
+            {
+
+                if (g < show)
+                {
+
+                    result = choices[g];
+                    g += 1;
+                }
+                else
+                {
+                    g = 0;
+                }
+
+            }
+            return result;
+        }
+    }
+}
---
+++ Vtitbid.ISP20.SarbaevZlokazov.RND/Program.cs
@@ -0,0 +1,5 @@
+﻿using Vtitbid.ISP20.SarbaevZlokazov.RND;
+Console.WriteLine("Введите промежуток рандома");
+int first = Convert.ToInt32(Console.ReadLine());
+int second = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Результат: {OwnRandom.Next(first,second)}");
